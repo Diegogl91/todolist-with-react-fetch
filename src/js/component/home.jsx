426ea9aaf0aa5bcd36106884025dbd29c20afd2f
Diegogl91 <garciadiego03@gmail.com>
2021-09-08 22:57:00 +0000

    Done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,21 +1,88 @@
-import React, { useState } from "react";
-import "/workspace/react-hello/src/styles/index.scss";
+import React, { useEffect, useState } from "react";
+import "/workspace/todo-list-with-react/src/styles/index.scss";
 
 //create your first component
 const Home = props => {
 	const [tasks, setTasks] = useState([]);
+	const url = "https://assets.breatheco.de/apis/fake/todos/user/Diegogl91";
 
 	const Enter = e => {
 		if (e.key === "Enter" && e.target.value !== "") {
-			tasks.push(e.target.value);
+			tasks.push({ label: e.target.value, done: false });
 			setTasks([...tasks]);
+			tasks.length == 1 ? createUser() : updateTask();
 			e.target.value = "";
 		}
 	};
 
+	useEffect(() => {
+		getTasks();
+	}, []);
+	const getTasks = () => {
+		fetch(url, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(response => {
+				return response.json();
+			})
+			.then(data => {
+				if (Array.isArray(data)) {
+					setTasks(data);
+				}
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	};
+
+	const createUser = () => {
+		fetch(url, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(tasks)
+		})
+			.then(response => response.json())
+			.then(data => {
+				updateTask();
+			})
+			.catch(error => console.log(error));
+	};
+
+	const updateTask = () => {
+		fetch(url, {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(tasks)
+		})
+			.then(response => response.json())
+			.then(data => {
+				console.log(data);
+			})
+			.catch(error => console.log(error));
+	};
+
+	const deleteTasks = () => {
+		fetch(url, {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(response => response.json())
+			.then(data => {})
+			.catch(error => console.log(error));
+	};
+
 	return (
 		<div className="titulo">
-			<h1>Todos</h1>
+			<h1>Todos List with Fetch</h1>
 			<div className="container d-flex justify-content-center text-align-center">
 				<div>
 					<input
@@ -33,12 +100,16 @@ const Home = props => {
 								return (
 									<div key={index}>
 										<li className="tareas">
-											<p>{t}</p>
+											<p>{t.label}</p>
 											<button
 												className="boton"
 												onClick={() => {
 													tasks.splice(index, 1);
 													setTasks([...tasks]);
+													console.log(tasks.length);
+													tasks.length === 0
+														? deleteTasks()
+														: updateTask();
 												}}>
 												<i className="fas fa-times"></i>
 											</button>
@@ -59,6 +130,14 @@ const Home = props => {
 					</ul>
 				</div>
 			</div>
+			<button
+				className="btn btn-danger"
+				onClick={() => {
+					setTasks([]);
+					deleteTasks();
+				}}>
+				Borrar Tareas
+			</button>
 		</div>
 	);
 };
